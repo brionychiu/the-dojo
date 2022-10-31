cd35081cdac00447ea27f379de57c0736e6066c4
brionychiu <yuikuta0618@gmail.com>
2022-10-31 23:04:09 +0800

    10/31

--- src/App.js
+++ src/App.js
@@ -2,16 +2,43 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 import './App.css'
 
+// pages & components 
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup"
+import Project from "./pages/project/Project";
+
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <div className="container">
+          <Switch>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/projects/:id">
+            <Project />
+          </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
+
   );
 }
 
