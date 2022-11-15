494a410040de76f4940830dc511acc7a5a13f372
brionychiu <yuikuta0618@gmail.com>
2022-11-15 22:59:14 +0800

    11/15 signup & logout

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,44 @@
+import { useState } from 'react'
+
 // styles 
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password)
+        }
+
     return ( 
-        <div>
-            
-        </div>
-     );
+        <form onSubmit={handleSubmit} className="auth-form">
+        <h2>Login</h2>
+        <label>
+        <span>email:</span>
+        <input
+            required 
+            type="email" 
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email}
+        />
+        </label>
+        <label>
+        <span>password:</span>
+        <input
+            required
+            type="password" 
+            onChange={(e) => setPassword(e.target.value)} 
+            value={password}
+        />
+        </label>
+        <button>login</button>
+        {/* {!isPending && <button className="btn">Sign up</button>}
+        {isPending && <button className="btn" disabled>loading</button>}
+        {error && <div className="error">{error}</div>} */}
+    </form>
+    );
 }
- 
+
 export default Login;
